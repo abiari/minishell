93155aa40c77fc70cf024562a2add6f5a4f8bce4
abiari <abiari@student.1337.ma>
2021-09-08 16:46:56 +0100

    norming ...

--- srcs/execution/pipeline.c
+++ srcs/execution/pipeline.c
@@ -6,7 +6,7 @@
 /*   By: abiari <abiari@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/26 17:35:24 by abiari            #+#    #+#             */
-/*   Updated: 2021/09/08 14:37:11 by abiari           ###   ########.fr       */
+/*   Updated: 2021/09/08 16:44:38 by abiari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,22 @@ char	*check_exec(char *cmd, t_list *envl)
 		bin = bin_path(bin, envl);
 	close(fd);
 	return (bin);
+}
+
+int	is_builtin(char *cmd)
+{
+	if ((ft_strcmp("echo", cmd) == 0) || (ft_strcmp("env", cmd) == 0)
+		|| (ft_strcmp("cd", cmd) == 0) || (ft_strcmp("export", cmd) == 0)
+		|| (ft_strcmp("pwd", cmd) == 0) || (ft_strcmp("exit", cmd) == 0)
+		|| (ft_strcmp("unset", cmd) == 0))
+		return (1);
+	else if ((ft_strcmp("ECHO", cmd) == 0) || (ft_strcmp("ENV", cmd) == 0)
+		|| (ft_strcmp("CD", cmd) == 0) || (ft_strcmp("EXPORT", cmd) == 0)
+		|| (ft_strcmp("PWD", cmd) == 0) || (ft_strcmp("EXIT", cmd) == 0)
+		|| (ft_strcmp("UNSET", cmd) == 0))
+		return (2);
+	else
+		return (0);
 }
 
 void	spawn_proc(int in, int *fd, t_pipeline *cmd, t_list **envl)
@@ -95,59 +111,11 @@ void	spawn_proc(int in, int *fd, t_pipeline *cmd, t_list **envl)
 	}
 }
 
-int	is_builtin(char *cmd)
+void	spawn_last(int in, int *fd, t_pipeline *cmd, t_list **envl)
 {
-	if ((ft_strcmp("echo", cmd) == 0) || (ft_strcmp("env", cmd) == 0)
-		|| (ft_strcmp("cd", cmd) == 0) || (ft_strcmp("export", cmd) == 0)
-		|| (ft_strcmp("pwd", cmd) == 0) || (ft_strcmp("exit", cmd) == 0)
-		|| (ft_strcmp("unset", cmd) == 0))
-		return (1);
-	else if ((ft_strcmp("ECHO", cmd) == 0) || (ft_strcmp("ENV", cmd) == 0)
-		|| (ft_strcmp("CD", cmd) == 0) || (ft_strcmp("EXPORT", cmd) == 0)
-		|| (ft_strcmp("PWD", cmd) == 0) || (ft_strcmp("EXIT", cmd) == 0)
-		|| (ft_strcmp("UNSET", cmd) == 0))
-		return (2);
-	else
-		return (0);
-}
-
-void	fork_pipes(t_pipeline *cmd, t_list **envl)
-{
-	int		in;
-	int		fd[2];
-	int		status;
-	int		ret;
 	char	**envp;
 
-	in = 0;
-	status = 0;
-	while (cmd->next != NULL)
-	{
-		ret = is_builtin(cmd->cmd[0]);
-		if (ret != 1)
-		{
-			cmd->cmd[0] = check_exec(cmd->cmd[0], *envl);
-			if (cmd->cmd[0] == NULL)
-				return ;
-		}
-		pipe(fd);
-		spawn_proc(in, fd, cmd, envl);
-		if (fd[1] > 2)
-			close(fd[1]);
-		if (in != 0)
-			close(in);
-		in = fd[0];
-		cmd = cmd->next;
-	}
-	ret = is_builtin(cmd->cmd[0]);
-	if (ret != 1)
-	{
-		cmd->cmd[0] = check_exec(cmd->cmd[0], *envl);
-		if (cmd->cmd[0] == NULL)
-			return ;
-	}
 	g_vars.pid = fork();
-	// g_vars.pid = 0;
 	if (g_vars.pid == 0)
 	{
 		if (in != 0)
@@ -187,10 +155,55 @@ void	fork_pipes(t_pipeline *cmd, t_list **envl)
 			exit(errno);
 		}
 	}
-	if (in != 0)
-		close(in);
+}
+
+int		check_if_builtin(t_pipeline *cmd, t_list **envl)
+{
+	int	ret;
+
+	ret = is_builtin(cmd->cmd[0]);
+		if (ret != 1)
+		{
+			cmd->cmd[0] = check_exec(cmd->cmd[0], *envl);
+			if (cmd->cmd[0] == NULL)
+				return (1);
+		}
+	return (0);
+}
+
+void	wait_exec(void)
+{
+	int	status;
+
+	status = 0;
 	while (waitpid(-1, &status, 0) > 0)
 		if (WIFEXITED(status))
 			g_vars.exit_code = WEXITSTATUS(status);
 }
-// wait for commands ";"
+
+void	fork_pipes(t_pipeline *cmd, t_list **envl)
+{
+	int		in;
+	int		fd[2];
+
+	in = 0;
+	while (cmd->next != NULL)
+	{
+		if (check_if_builtin(cmd, envl))
+			return ;
+		pipe(fd);
+		spawn_proc(in, fd, cmd, envl);
+		if (fd[1] > 2)
+			close(fd[1]);
+		if (in != 0)
+			close(in);
+		in = fd[0];
+		cmd = cmd->next;
+	}
+	if (check_if_builtin(cmd, envl))
+			return ;
+	spawn_last(in, fd, cmd, envl);
+	if (in != 0)
+		close(in);
+	wait_exec();
+}
